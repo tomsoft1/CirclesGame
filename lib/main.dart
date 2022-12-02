1fe3537cf7e859a5ed70c949404c9c5bfe4499ad
Thomas Landspurg <thomas.landspurg@gmail.com>
2022-12-02 18:54:42 +0100

    Imrpove some graph rendering

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print
 
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
@@ -19,12 +18,27 @@ class Player extends PositionComponent {
   static final _paint = Paint()
     ..color = Colors.blueGrey
     ..strokeWidth = 2.0;
-
+  static final _clear = Paint()
+    ..color = Color.fromARGB(212, 146, 76, 76)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2.0;
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    canvas.drawCircle(Offset.zero, width, _paint);
-    //  canvas.drawRect(size.toRect(), _paint);
+    Path path = Path();
+    path.addOval(Rect.fromCircle(center: Offset.zero, radius: width));
+    Path pathRemove = Path();
+
+    for (int i = 0; i < 12; i++) {
+      Vector2 dir = Vector2(width, 0);
+      dir.rotate(i.toDouble() * 2 * pi / 12);
+      path.addOval(
+          Rect.fromCircle(center: Offset(dir.x, dir.y), radius: width / 5));
+    }
+    pathRemove.addOval(Rect.fromCircle(center: Offset.zero, radius: width / 2));
+    Path result = Path.combine(PathOperation.difference, path, pathRemove);
+    canvas.drawPath(result, _paint);
+    canvas.drawPath(result, _clear);
   }
 
   void move(Vector2 delta) {
@@ -43,26 +57,29 @@ class Power extends PositionComponent {
   }
 }
 
-class Arrow extends PositionComponent {
-  static final _paint = Paint()
-    ..color = const Color.fromARGB(255, 240, 107, 98)
-    ..strokeWidth = 2.0;
-  static final _paintInner = Paint()..color = Color.fromARGB(255, 238, 255, 0);
+class Arrow extends PositionComponent with HasPaint {
+  Arrow() {
+    paint.color = const Color.fromARGB(255, 26, 15, 14);
+    paint.strokeWidth = 2.0;
+  }
+
+  static final _paintInner = Paint()..color = Color.fromARGB(255, 255, 136, 0);
   double power = 0;
   @override
   void render(Canvas canvas) {
     super.render(canvas);
     Path path = Path();
-    path.moveTo(0, width * 1.2);
-    path.lineTo(-width, width * 1.1);
+    double arrowWidth = width / 2;
+    path.moveTo(0, width * 1.4);
+    path.lineTo(-arrowWidth, width * 1.1);
     path.lineTo(0, height);
-    path.lineTo(width, width * 1.2);
+    path.lineTo(arrowWidth, width * 1.2);
     path.close();
     Path circle = Path();
     circle.addOval(Rect.fromCircle(
         center: Offset.zero, radius: width + power * (height - width) / 100));
     var resulting = Path.combine(PathOperation.intersect, path, circle);
-    canvas.drawPath(path, _paint); //  canvas.drawRect(size.toRect(), _paint);
+    canvas.drawPath(path, paint); //  canvas.drawRect(size.toRect(), _paint);
     canvas.drawPath(
         resulting, _paintInner); //  canvas.drawRect(size.toRect(), _paint);
   }
@@ -77,15 +94,15 @@ class PowerEffect extends Effect with EffectTarget<Arrow> {
 
   @override
   void apply(double progress) {
-    target.power = progress * 100;
+    target.power = progress.abs() * 100;
   }
 }
 
 class Platform extends PositionComponent {
   static final _paint = Paint()..color = Colors.red;
-  static final _paintInner = Paint()..color = Colors.orange;
-  static final _paintTarget = Paint()..color = Color.fromARGB(255, 255, 251, 0);
-
+  static final _paintInner = Paint()
+    ..color = Color.fromARGB(255, 239, 236, 240);
+  static final _paintTarget = Paint()..color = Color.fromARGB(255, 255, 174, 0);
   late int platformNumber;
   Platform(int number) {
     platformNumber = number;
@@ -93,10 +110,11 @@ class Platform extends PositionComponent {
     width = 50;
     height = 50;
     final displayNum = TextComponent(
+        position: Vector2(100, -15),
         textRenderer: TextPaint(
             style: GoogleFonts.lato(
           fontSize: 35,
-          color: const Color.fromARGB(255, 105, 103, 104),
+          color: const Color.fromARGB(100, 105, 103, 104),
         )),
         text: "$platformNumber");
     add(displayNum);
@@ -104,16 +122,15 @@ class Platform extends PositionComponent {
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    var smallSize = 0.9 * width;
+    var smallSize = 0.8 * width;
     var targetSize = 0.4 * width;
-
+    canvas.drawCircle(Offset.zero, width * 1.02, Paint()..color = Colors.white);
     canvas.drawCircle(Offset.zero, width, _paint);
     canvas.drawCircle(Offset.zero, smallSize, _paintInner);
-    canvas.drawCircle(Offset.zero, targetSize, _paintTarget);
+    canvas.drawCircle(Offset.zero, targetSize, _paint);
+    canvas.drawCircle(Offset.zero, targetSize * 0.9, _paintTarget);
   }
 }
-
-const nbPlatforms = 10;
 
 class CirclesGame extends FlameGame with TapDetector {
   late Player player;
@@ -123,7 +140,17 @@ class CirclesGame extends FlameGame with TapDetector {
   late PowerEffect powerEffect;
   late TextComponent scoreCounter;
   int score = 0;
+  static final textPaint = TextPaint(
+      style: GoogleFonts.lato(
+          fontSize: 35, color: const Color.fromARGB(255, 105, 103, 104)));
+
+  int nbPlatforms = 10;
+  OpacityEffect opacityEffect =
+      OpacityEffect.to(0.0, EffectController(duration: 2), onComplete: () {
+    print("Opacity completed");
+  });
   int targetPlatform = 1;
+  Random r = Random();
 
   List<Platform> platforms = [];
   var range = pi / 3;
@@ -134,7 +161,7 @@ class CirclesGame extends FlameGame with TapDetector {
   ], infinite: true);
 
   @override
-  Color backgroundColor() => Colors.white;
+  Color backgroundColor() => Color.fromARGB(255, 230, 241, 233);
 
   @override
   Future<void> onLoad() async {
@@ -154,33 +181,13 @@ class CirclesGame extends FlameGame with TapDetector {
 
     arrow = Arrow()
       ..width = player.size.toSize().width
-      ..height = player.size.toSize().width * 4
+      ..height = player.size.toSize().width * 6
       ..anchor = Anchor.topLeft;
     refArrow = Arrow()
       ..width = player.size.toSize().width
       ..height = player.size.toSize().width * 4
       ..anchor = Anchor.topLeft;
-    Random r = Random();
-    Vector2 currentPos = Vector2(200.0, 200.0);
-    for (int i = 0; i < nbPlatforms; i++) {
-      var platform = Platform(i);
-
-      platform.position = currentPos.clone();
-      double dist = 200 + r.nextInt(200).toDouble();
-      double angle = r.nextDouble() * pi / 2 - pi + pi / 4;
-//      angle = pi;
-//      dist = 100.0;
-      Vector2 v = Vector2(dist, 0.0);
-      v.rotate(angle);
-      currentPos.add(v);
-      print("Angle:$angle Dist:$dist currentPos:$currentPos");
-
-      platform.anchor = Anchor.topLeft;
-      platform.width = 60 + r.nextInt(30).toDouble();
-      platform.height = platform.width;
-      platforms.add(platform);
-      add(platform);
-    }
+    initLevel(10);
     final textStyle = GoogleFonts.lato(
       fontSize: 35,
       color: const Color.fromARGB(255, 105, 103, 104),
@@ -198,37 +205,38 @@ class CirclesGame extends FlameGame with TapDetector {
 
     add(player);
 
-    player.position = platforms.first.position;
 //    player.position.add(Vector2(-20.0, -40.0));
     player.add(arrow);
 //    player.add(refArrow);
-    camera.followComponent(player);
+    camera.followComponent(player, relativeOffset: const Anchor(0.5, 0.8));
 
     setTarget(targetPlatform);
     print("After:");
-    arrow.add(allEffect);
   }
 
   @override
   void onTapDown(TapDownInfo info) {
     print("Tap down: ${toDeg(arrow.angle)}");
-    arrow.remove(allEffect);
-
-    try {
-      arrow.add(powerEffect);
-    } catch (e) {}
+    if (allEffect.parent != null) arrow.remove(allEffect);
+    if (powerEffect.parent == null) arrow.add(powerEffect);
   }
 
   @override
   void onTapUp(TapUpInfo info) {
     print("tap up");
+
+    // Compute the destination of the player based on the power of the arrrow
     var dist = 10 + 5 * arrow.power;
-    try {
-      arrow.remove(powerEffect);
-      arrow.power = 0;
-    } catch (e) {
-      print('Tring to remove, error: $e');
+    //Remove the arrow power
+    if (powerEffect.parent != null) arrow.remove(powerEffect);
+    arrow.power = 0;
+    if (opacityEffect.parent == null) {
+      arrow.add(opacityEffect);
+      opacityEffect.reset();
+    } else {
+      opacityEffect.reset();
     }
+    // Compute the desitnation of the player
     print('power dist:$dist');
     var dx = dist * cos(arrow.angle + pi / 2);
     var dy = dist * sin(arrow.angle + pi / 2);
@@ -237,34 +245,79 @@ class CirclesGame extends FlameGame with TapDetector {
         onComplete: () {
       var target = platforms[targetPlatform];
       var dist = target.position.distanceTo(player.position) - target.width;
+      arrow.setAlpha(255);
       print('Distance:$dist');
       if (dist < 0) {
         print('You,ve made it....');
         targetPlatform = targetPlatform + 1;
+        score += 1;
         if (targetPlatform == nbPlatforms) {
           print("Level Ended!");
-          targetPlatform = 1;
+          nextLevel();
         }
         setTarget(targetPlatform);
-        arrow.add(allEffect);
+        if (allEffect.parent == null) arrow.add(allEffect);
+        TextComponent textWellDone =
+            TextComponent(text: "Well done", textRenderer: textPaint);
+        textWellDone.add(RemoveEffect(delay: 1.0));
+/*        textWellDone.add(SequenceEffect([
+          SizeEffect.to(Vector2(1.1, 1.1), EffectController(duration: 0.5)),
+          RemoveEffect()
+        ]));*/
+        player.add(textWellDone);
         scoreCounter.text = "Score:$score Num:$targetPlatform";
       } else {
-        setTarget(targetPlatform);
-        arrow.add(allEffect);
-//        resetGame();
+        player.add(ScaleEffect.to(
+            Vector2.zero(),
+            EffectController(
+              duration: 1.0,
+            ), onComplete: () {
+          resetGame();
+        }));
       }
     }));
   }
 
+  initLevel(int maxPlatforms) {
+    nbPlatforms = maxPlatforms;
+
+    Vector2 currentPos = Vector2(200.0, 200.0);
+    for (int i = 0; i < nbPlatforms; i++) {
+      var platform = Platform(i);
+      platform.position = currentPos.clone();
+      double dist = 200 + r.nextInt(200).toDouble();
+      double angle = r.nextDouble() * pi / 2 - pi + pi / 4;
+
+      Vector2 v = Vector2(dist, 0.0);
+      v.rotate(angle);
+      currentPos.add(v);
+
+      platform.anchor = Anchor.topLeft;
+      platform.width = 60 + r.nextInt(30).toDouble();
+      platform.height = platform.width;
+      platforms.add(platform);
+      add(platform);
+    }
+    player.position = platforms.first.position;
+    if (allEffect.parent == null) arrow.add(allEffect);
+  }
+
   resetGame() {
+    score = -10;
+    nextLevel();
+  }
+
+  nextLevel() {
+    score += 10;
     targetPlatform = 1;
+    player.scale = Vector2(1.0, 1.0);
     player.position = platforms[0].position.clone();
     setTarget(targetPlatform);
+    if (allEffect.parent == null) arrow.add(allEffect);
   }
 
   @override
   void onPanUpdate(DragUpdateInfo info) {
-    print("here");
     player.move(info.delta.game);
   }
 
